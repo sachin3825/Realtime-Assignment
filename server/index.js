a8d87d0385601753c90a79c6e8d4b1e16200b9ad
sachin3825 <sachinkumawat55@gmail.com>
2023-10-08 20:49:21 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://realtime-assignment.vercel.app/",
     tempFileDir: "/temp",
   })
 );
